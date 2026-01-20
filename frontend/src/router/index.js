1a6d5b7439eec85334ce5d7acbbff2ad1a124f27
Meo <ohm132548@gmail.com>
2026-01-20 18:02:24 +0700

    Update index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Login from '../components/Login.vue';
+import Register from '../components/Register.vue';
+import Dashboard from '../components/Dashboard.vue';        
+import Homepage from '../components/Homepage.vue';
+import AdminDashboard from '../components/AdminDashboard.vue';
+
+
+const routes = [ createRouter
+  {
