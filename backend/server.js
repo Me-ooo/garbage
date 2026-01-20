c33a3045f6f3ce31a909f1c08930f02a6850fb90
Meo <ohm132548@gmail.com>
2026-01-20 13:26:39 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use('/api/admin', require('./routes/admin'));
 
-module.exports = app;
